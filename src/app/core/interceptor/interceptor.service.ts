6d3da08d0c76607ca2975475ee7502e54af93514
davidmazar <davidmazar@hotmail.com>
2022-12-17 18:12:13 -0500

    Se efectua el cambio en la vista de la creacion del programa para que cree las denominaciones conforme a las modalidades seleccionadas

--- src/app/core/interceptor/interceptor.service.ts
+++ src/app/core/interceptor/interceptor.service.ts
@@ -19,7 +19,7 @@ constructor(private routes:Router, private spinnerService:SpinnerService) { }
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
 
     this.spinnerService.show() 
-    const token: string | null =('eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJydWJlbi5hcmF1am8iLCJhdXRob3JpdGllcyI6WyJST0xFX0NfQ1JFRCIsIlJPTEVfRF9DUkVEIiwiUk9MRV9SX0NSRUQiXSwiZW1haWwiOiJydWJlbi5hcmF1am9AdW5pc2ltb24uZWR1LmNvIiwic3R1ZGVudF9pZCI6MjE4Niwic3R1ZGVudF9jb2RlIjoxMDQ4Mjk0NDcxLCJuYW1lIjoiQVJBVUpPICBSVUJFTiAiLCJwbGFjZV9pZCI6MTAxLCJwbGFjZV9jb2RlIjoiQkFSIiwidXNlcl90eXBlIjoiRVNUVURJQU5URSIsImlhdCI6MTY2Njg4MzQwMiwiZXhwIjoxNjY5NDc1NDAyfQ.7L11pe8ZKfTI3wqqCzZnol2M0BZ5zKW083nkkvTxzN4');
+    const token: string | null =('eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJydWJlbi5hcmF1am8iLCJhdXRob3JpdGllcyI6WyJST0xFX0NfQ1JFRCIsIlJPTEVfRF9DUkVEIiwiUk9MRV9SX0NSRUQiXSwiZW1haWwiOiJydWJlbi5hcmF1am9AdW5pc2ltb24uZWR1LmNvIiwic3R1ZGVudF9pZCI6MjE4Niwic3R1ZGVudF9jb2RlIjoxMDQ4Mjk0NDcxLCJuYW1lIjoiQVJBVUpPICBSVUJFTiAiLCJwbGFjZV9pZCI6MTAxLCJwbGFjZV9jb2RlIjoiQkFSIiwidXNlcl90eXBlIjoiRVNUVURJQU5URSIsImlhdCI6MTY3MDc4NzQxMywiZXhwIjoxNjczMzc5NDEzfQ.ViCG3PGd7Ab7-NsHcXp6bJ-l2JBZkQO1Rl0hsczD-cA');
 
     const type_token: string | null = ('Bearer');
    
